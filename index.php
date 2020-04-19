a692c5baefcdb9421fa84fc883442d982ad3d272
redskip1114 <63499447+redskip1114@users.noreply.github.com>
2020-04-19 16:39:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -271,8 +271,17 @@ article.reply::before {
 </head>
 <body>
 <h1>ひと言掲示板</h1>
-<!-- ここにメッセージの入力フォームを設置 -->
-
+<form method="post">
+	<div>
+		<label for="view_name">表示名</label>
+		<input id="view_name" type="text" name="view_name" value="">
+	</div>
+	<div>
+		<label for="message">ひと言メッセージ</label>
+		<textarea id="message" name="message"></textarea>
+	</div>
+	<input type="submit" name="btn_submit" value="書き込む">
+</form>
 <hr>
 <section>
 <!-- ここに投稿されたメッセージを表示 -->
